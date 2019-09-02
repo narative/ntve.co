43adf5497db063651bd9bc0d99fb4e152440b914
brotzky <brotzky@gmail.com>
2019-09-02 11:01:31 -0700

    Adding base

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-const dotenv = require('dotenv');
-const contentful = require('contentful');
-const fs = require('fs');
-const path = require('path');
+const dotenv = require("dotenv");
+const contentful = require("contentful");
+const fs = require("fs");
+const path = require("path");
 
 dotenv.config();
 
-const BASE_URL = 'https://narative.co';
+const BASE_URL = "https://narative.co";
 
-const root = path.dirname('./');
-const redirectPath = path.join(root, '_redirects');
+const root = path.dirname("./");
+const redirectPath = path.join(root, "_redirects");
 
 // Create a clinet to talk with Contentful
 const client = contentful.createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_PREVIEW_API_KEY,
-  host: 'preview.contentful.com',
-  environment: process.env.CONTENTFUL_ENVIRONMENT || 'master',
-  dynamic_antries: 'auto',
+  host: "preview.contentful.com",
+  environment: process.env.CONTENTFUL_ENVIRONMENT || "master",
+  dynamic_antries: "auto"
 });
 
 async function createRedirects(event, context) {
   const entries = await client.getEntries({
     include: 10,
-    limit: 1000,
+    limit: 1000
   });
 
   const articles = entries.items.filter(
-    entry => entry.sys.contentType.sys.id === 'article',
+    entry => entry.sys.contentType.sys.id === "article"
   );
 
   const redirects = articles.reduce((curr, next) => {
@@ -34,7 +34,7 @@ async function createRedirects(event, context) {
     const longUrl = `${BASE_URL}/articles/${slug}`;
 
     return `/${shortUrl} ${longUrl}\n` + curr;
-  }, `/ ${BASE_URL}\n`);
+  }, `/irap https://irap.narative.co\n /* ${BASE_URL}\n`);
 
   console.log(redirects);
   fs.writeFileSync(redirectPath, redirects);
